7eee3b5bddb4303cefa878107adbfc7527583707
xxwvlyx <alwaysxjun@gmail.com>
2025-09-29 18:33:40 +0000

    Update script.bash

--- script.bash
+++ script.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo find /etc -type f | sudo wc -
+if [ "$EUID" -ne 0 ]; then
+	echo "You are not root." >&2
+	exit 1
+fi 
+echo "Hello, root"
+count=$(find /etc -type f | wc -l)
+echo "Number of regular files in /etc: $count"
